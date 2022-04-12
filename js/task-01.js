df6d1d7a0af09cb20947764af76a4def8fb988df
Alex <gryshchenko.oleksii@gmail.com>
2022-04-12 12:44:44 +0300

    Task 1

--- js/task-01.js
+++ js/task-01.js
@@ -2,5 +2,12 @@ const quantityLiItemEl = document.querySelectorAll('.item');
 const lengthLiItemEl = quantityLiItemEl.length;
 console.log(`Number of categories: ${lengthLiItemEl}`);
 
+quantityLiItemEl.forEach(elem => {
+    const headText = elem.firstElementChild.textContent;
+    console.log(`Category: ${headText}`);
 
-  
+    const elementsOfCategoty = elem.lastElementChild;
+    const quantutyElementsOfCategory = elementsOfCategoty.querySelectorAll('li');
+    console.log(`Elements: ${quantutyElementsOfCategory.length}`);
+})
+
