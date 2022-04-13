1dae64e12ea0220cc34fb971eae91f0da2708c95
Alex <gryshchenko.oleksii@gmail.com>
2022-04-13 11:19:41 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('ul');
+ingredients.map(ingredient => {
+  const listIngredientsItem = document.createElement('li');
+  listIngredientsItem.classList.add('item');
+  listIngredientsItem.textContent = ingredient;
+  listIngredients.append(listIngredientsItem);
+});
