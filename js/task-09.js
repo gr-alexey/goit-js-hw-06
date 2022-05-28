fcf5a94862c04f46e3713e1cde76a425f6536a1b
Alex <gryshchenko.oleksii@gmail.com>
2022-05-28 16:21:20 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector('button');
+const page = document.querySelector('body');
+const color = document.querySelector('span');
+
+button.addEventListener('click', changeColor);
+
+function changeColor(e) {
+  page.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+};
