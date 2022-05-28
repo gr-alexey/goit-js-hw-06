63e4ab1a6bf235126f39f0a82543e46b95c78712
Alex <gryshchenko.oleksii@gmail.com>
2022-05-28 15:54:22 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) { 
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const userEmail = e.currentTarget.elements.email.value;
+    const userPassword = e.currentTarget.elements.password.value;
+
+    if (userEmail === '' || userPassword === '') {
+    alert('Все поля должны быть заполнены!');
+    } else {
+        let user = {
+            email: '',
+            password: '',
+        }
+        user.email = e.currentTarget.elements.email.value;
+        user.password = e.currentTarget.elements.password.value;
+        console.log(user)
+    }
+    form.reset();
+};
